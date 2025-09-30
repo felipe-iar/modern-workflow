50083b11397b7ccfb17bddbe94643c604c1f90d5
Felipe Torrezan <felipe.torrezan@iar.com>
2025-09-30 13:16:51 +0200

    Apt install jlink

--- .devcontainer/jlink/install.sh
+++ .devcontainer/jlink/install.sh
@@ -2,4 +2,4 @@
 
 wget --post-data "accept_license_agreement=accepted" https://www.segger.com/downloads/jlink/JLink_Linux_x86_64.deb
 
-dpkg -i ./JLink_Linux_x86_64.deb
+apt install ./JLink_Linux_x86_64.deb
